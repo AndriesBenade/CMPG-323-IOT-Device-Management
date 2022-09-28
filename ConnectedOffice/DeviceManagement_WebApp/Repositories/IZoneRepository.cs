af66279ffd6e04aaa6ba1f135777f8fc5ad39791
Andries Benade <benadeandries@gmail.com>
2022-09-28 15:27:48 +0200

    Added repository classes

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repositories/IZoneRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeviceManagement_WebApp.Repositories
+{
+    public class IZoneRepository
+    {
+        public Guid ZoneID { get; set; }
+        public string ZoneName { get; set; }
+        public string ZoneDescription { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
